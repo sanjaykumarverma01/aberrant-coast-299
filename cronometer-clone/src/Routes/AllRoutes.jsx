c9d84e65de67f3c86d2bce2792900ce48df2a403
Rohit sahu <96680038+RohitSahuvlog@users.noreply.github.com>
2022-09-29 09:53:11 +0530

    done

--- cronometer-clone/src/Routes/AllRoutes.jsx
+++ cronometer-clone/src/Routes/AllRoutes.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
+import Login from "../Components/login";
+import Signin from '../Components/signin';
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    
+    <Routes>
+
+      <Route path="/login" element={<Login/>}>
+
+      </Route>
+      
+      <Route path="/signin" element={<Signin/>}>
+
+      </Route>
+
+    </Routes>
   )
 }
 
