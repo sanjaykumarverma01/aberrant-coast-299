3efc16575a2e51601fb7fe7bd8afd3bf9efa386c
Sanjaykumar Verma <vermasanjaykumar97@gmail.com>
2022-09-29 09:41:33 +0530

    Update AllRoutes.jsx

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -5,11 +5,8 @@ import Dairy from '../Components/AfterLogin/pages/Dairy'
 const AllRoutes = () => {
   return (
     <div>
-      <h1>nothing showing on screen</h1>
       <Routes>
         <Route path="/" element={<Dairy/>}/>
-        <Route/>
-        <Route/>
       </Routes>
     </div>
   )
