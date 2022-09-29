eea5f43bdda5f2805ed725d10d427ddd74c9e0a7
Sanjaykumar Verma <vermasanjaykumar97@gmail.com>
2022-09-29 09:47:21 +0530

    Update AllRoutes.jsx

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Dairy from '../Components/AfterLogin/pages/Dairy'
 
 const AllRoutes = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Dairy/>}/>
-      </Routes>
+      <h1>Routes</h1>  
     </div>
   )
 }
