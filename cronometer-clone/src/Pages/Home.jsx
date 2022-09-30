96ca2cbd7a11b192fbcc41b1e5a5261100f6ebfc
SK-WebDeveloper <vermasanjaykumar97@gmail.com>
2022-09-30 18:07:48 +0530

    added home

---
+++ cronometer-clone/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+const Home = () => {
+  return (
+    <Box>
+      <Navbar />
+      <Footer />
+    </Box>
+  );
+};
+
+export default Home;
