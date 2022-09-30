ab8ad1f7f422ea0f54be84c9cba9685fde5eccda
swapnil ramteke <97676520+swapnilpreet@users.noreply.github.com>
2022-09-30 17:29:21 +0530

    most of work complete on landing page after login

--- cronometer-clone/src/Components/AfterLogin/pages/Foods.jsx
+++ cronometer-clone/src/Components/AfterLogin/pages/Foods.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import InsideNavbar from '../../InsideNavbar';
 import InsideFooter from '../../InsideFooter';
 import styles from '../css/food.module.css'
+import Bar_chart from './Bar_chart';
 const Foods = () => {
   return (
     <div>
       <InsideNavbar/>
       <div className={styles.food}>
-                working area 
+                {/* working area  */}
+                <Bar_chart/>
       </div>
       <InsideFooter/>
     </div>
