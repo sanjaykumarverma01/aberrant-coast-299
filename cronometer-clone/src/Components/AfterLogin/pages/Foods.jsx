45c4d5247f7090e4ad180e649c32a500e55dea2d
swapnil ramteke <97676520+swapnilpreet@users.noreply.github.com>
2022-10-01 18:53:41 +0530

    added calender logout section

--- cronometer-clone/src/Components/AfterLogin/pages/Foods.jsx
+++ cronometer-clone/src/Components/AfterLogin/pages/Foods.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import InsideNavbar from '../../InsideNavbar';
 import InsideFooter from '../../InsideFooter';
 import styles from '../css/food.module.css'
-import Bar_chart from './Bar_chart';
 const Foods = () => {
   return (
     <div>
       <InsideNavbar/>
       <div className={styles.food}>
                 {/* working area  */}
-                <Bar_chart/>
+                {/* <Bar_chart/> */}
       </div>
       <InsideFooter/>
     </div>
--- cronometer-clone/src/Components/InsideNavbar.jsx
+++ cronometer-clone/src/Components/InsideNavbar.jsx
@@ -1,12 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from './Innavbar.module.css';
-
-
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Button,
+  Portal,
+  Flex,
+  Grid,
+  Box,
+  GridItem,
+  Text,
+} from '@chakra-ui/react'
+import {IoMdArrowDropdown, IoMdArrowDropup} from 'react-icons/io'
 const InsideNavbar = () => {
+  const [open, setopen] = useState(false)
+
+
+  const handleArrow = () => {
+    let status = !open;
+    setopen(status);
+  };
+
   return (
     <div className={styles.navbar}>
+      <div style={{display:"flex" ,border:"1px solid red"}} >
         <img className={styles.logo} src="https://cdn1.cronometer.com/logos/cronometer-logo-orange.png" alt="" />
+        
+          <div style={{paddingTop:"30px"}}>
+          <Popover >
+            <PopoverTrigger>
+              <Button color="orange.500" size="lg" onClick={handleArrow}>{open ? <IoMdArrowDropdown/> : <IoMdArrowDropup/>}</Button>
+            </PopoverTrigger>
+            <Portal >
+              <PopoverContent w="550px" h='300px' boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px" ml='100px'>
+                <PopoverArrow />
+                <Flex>
+                <PopoverHeader display="flex">Signed in as: swapnilramteke004@gmail.com</PopoverHeader>
+                <PopoverCloseButton />
+                <PopoverBody>
+                <Button colorScheme='orange' variant='outline' ml='30px'>
+                    Logout
+                  </Button>
+                </PopoverBody>
+                </Flex>
+                <Grid templateColumns='repeat(2, 1fr)' gap={6} mt='20px'>
+                  <GridItem border='1px solid white'  _hover={{border:"1px solid rgb(221, 99, 17)"}} pl='20px'>
+                    <Flex  gap={20}>
+                      <Text fontSize='2xl'>Gold</Text>
+                      <Text fontSize='sm'>$49.99USD/Year</Text>
+                    </Flex>
+                    <Box>
+                      <Text fontSize='xs' mt='20px'>Our primimum features likes timestamps and food suggestion will help you plan customers and share your prefect diet</Text>
+                    </Box>
+                    <Link to="/plan" border='1px solid red'>
+                      <Text ml='60%' mt='70px' color='rgb(221, 99, 17)'>Learn More</Text>
+                      </Link>
+                  </GridItem >
+
+
+                  <GridItem border='1px solid white'  _hover={{border:"1px solid rgb(221, 99, 17)"}} pl='20px'>
+                    <Flex  gap={10}>
+                      <Text fontSize='2xl'>Professional</Text>
+                      <Text fontSize='sm'>$29.95/mo</Text>
+                    </Flex>
+                    <Box>
+                      <Text fontSize='xs' mt='20px'>Are you a Coach or Healthcare professional that believes diet is a critical compoenets heatlh and fitness ? Learn how you can monitor clients nutrients in real time with Cronamerter pro</Text>
+                    </Box>
+                    <Link to="/plan" border='1px solid red'>
+                      <Text ml='60%' mt='40px' color='rgb(221, 99, 17)'>Learn More</Text>
+                      </Link>
+                  </GridItem >
+                </Grid>
+              </PopoverContent>
+            </Portal>
+          </Popover>
+          </div>
+      </div>
+
       <div className={styles.links}>
         <Link className={styles.link} to="/dairy"><h4>Diary</h4></Link>
         <Link className={styles.link} to="/trends"><h4>Trends</h4></Link>
@@ -15,9 +93,8 @@ const InsideNavbar = () => {
         <Link className={styles.link} to="/plans"><h4>Plans</h4></Link>
         <Link className={styles.link} to="/helps"><h4>Help</h4></Link>
       </div>
-      <div>
 
-      </div>
+
     </div>
   );
 };
